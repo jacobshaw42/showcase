b5dfac524b25e97fd3e142e591da8c3313b4b381
jacobshaw42 <jacob.shaw42@gmail.com>
2021-12-10 09:47:42 -0600

    Utilizes pandas package more to simplify the code

--- api-log.py
+++ api-log.py
@@ -10,39 +10,33 @@ api_list=list(reader(api_log))
 #print(api_list[:3])
 
 #using list comprehension, split the second column and take the last element to get the messages
-data=[[x[0]+'.'+x[1][:3],x[1].split(' "')[-2][:-1],x[1].split('" "')[-1][:-1]] for x in api_list]
-#print(data[:3])
+start=[[x[0]+'.'+x[1][:3],x[1].split(' "')[-2][:-1],x[1].split('" "')[-1][:-1]] for x in api_list if 'start' in x[1].split('" "')[-1]]
+end=[[x[0]+'.'+x[1][:3],x[1].split(' "')[-2][:-1],x[1].split('" "')[-1][:-1]] for x in api_list if 'end' in x[1].split('" "')[-1]]
+#print(start[:3],len(start))
+#print(end[:3],len(start))
 
-#use dictionary to put associated start-end time together
-dict1={}
-for x in data:
-    if x[1] not in dict1:
-        dict1[x[1]]=[x[0],x[2]]
-    else:
-        dict1[x[1]]+=(x[0],x[2])
-#print(len(dict1))
-#print(dict1)
-
-#create message, send message, start, end times into pandas friendly list
-data2=[]
-for v in dict1.values():
-    msg=v[1]+' - '+v[3]
-    start=v[0]
-    end=v[2]
-    data2.append([msg,start,end])
-#print(data2,len(data2))
-
-#import pandas to and create dataframe using the new data2 list
+#import pandas and create start df and end df containing times, key, and message
 import pandas as pd
-df=pd.DataFrame(data2, columns=['Log message','Start Time','End Time'])
+df_start=pd.DataFrame(start, columns=['Start Time','key','Start message'])
+df_end=pd.DataFrame(end, columns=['End Time','key','End message'])
+#print(df_start)
+#print(df_end)
+
+#join start and end df using the key columns
+df_final=df_start.set_index('key').join(df_end.set_index('key')).reset_index()
+#print(df_final)
+
+#combine the start and end messages into log message column
+df_final['Log message']=df_final['Start message']+' - '+df_final['End message']
+#print(df_final)
 
 #change time columns into datetime for datetime computation, create  time diff column
-df['Start Time']=pd.to_datetime(df['Start Time'],format='%Y-%m-%d %H:%M:%S.%f')
-df['End Time']=pd.to_datetime(df['End Time'],format='%Y-%m-%d %H:%M:%S.%f')
-df['Time Diff']=(df['End Time']-df['Start Time']).astype('timedelta64[ms]')/1000
-#print(df['Log message'])
-#print(df[['Start Time','End Time','Time Diff']])
-#print(df)
+df_final['Start Time']=pd.to_datetime(df_final['Start Time'],format='%Y-%m-%d %H:%M:%S.%f')
+df_final['End Time']=pd.to_datetime(df_final['End Time'],format='%Y-%m-%d %H:%M:%S.%f')
+df_final['Time Diff']=(df_final['End Time']-df_final['Start Time']).astype('timedelta64[ms]')/1000
+#print(df_final['Log message'])
+#print(df_final[['Log message','Start Time','End Time','Time Diff']])
+#print(df_final)
 
 #send df to csv, no index needed
-df.to_csv('log-to-csv.csv',index=False)
+df_final[['Log message','Start Time','End Time','Time Diff']].to_csv('log-to-csv.csv',index=False)
